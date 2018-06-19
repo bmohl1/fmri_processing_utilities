c65046c131aa97483d07080d922c4c95991e109e
bmohl <bmohl@thalamus.ucdenver.pvt>
2018-06-19 09:58:27 -0600

    Made ART input file name flexible.

--- art_mtncorr.m
+++ art_mtncorr.m
@@ -39,7 +39,7 @@ end
 projName = textscan(cwd,'%s','Delimiter','/');
 if strcmp(subjs,projName{1,1}{end})
 projName = projName{1,1}{end-1};
-else   
+else
 projName = projName{1,1}{end};
 end
 
@@ -54,13 +54,13 @@ for iSubj = 1:length(pth_subjdirs)
     else
     pth = pth{1,1}(2:spIx+1);
     end
-    
-    [proj_dir subj unk] = fileparts(strtrim(sprintf('/%s',pth{:}))); 
-    
+
+    [proj_dir subj unk] = fileparts(strtrim(sprintf('/%s',pth{:})));
+
     %for iTask = 1:length(taskArray);
         task    = pth_taskdirs.task; %stored from file_selector_task
         rawDirName = pth_taskdirs.rawDir; % if "raw" exists in file structure
-        
+
         if isempty(glob(char(strcat(pth_subjdirs{iSubj}, filesep,task,filesep,'*_art_graphs*')))); % has the ART correction already been applied?
             fprintf('Processing %s task %d \n', subj);
             tmp = strfind(pth,subj); % Scan all the parts of the path to find which pieces should be put together for the raw directory path
@@ -70,12 +70,12 @@ for iSubj = 1:length(pth_subjdirs)
             else
                 raw_dir = strcat(strtrim(sprintf('/%s',pth{:})),filesep,task, filesep, rawDirName);
             end
-            
-            
+
+
             find_files = rdir(strcat(raw_dir,filesep,'w','*.nii'));
             findShort  = cellfun(@(x) numel(x),{find_files.name}); %compare the length of all the nii's
             find_files = find_files(findShort==min(findShort));
-            
+
             [ ~, ~, ext ] =fileparts(find_files(1).name);
             if strcmp (ext, '.nii')
                 ftmp = {};
@@ -90,12 +90,12 @@ for iSubj = 1:length(pth_subjdirs)
                     swFiles{1,iSwF} = fullfile(find_files(iSwF).name);
                 end
             end
-            
+
             if numel(swFiles) > 0
                 %% Load parameters
                 fileName = (strcat(raw_dir,filesep,subj,'_art.cfg'));
                 fid = fopen(fileName,'w+');
-                
+
                 fprintf(fid, 'sessions: 1\n' );
                 fprintf(fid, 'global_mean: 1\n');
                 % global mean type (1: Standard 2: User-defined mask)
@@ -107,7 +107,7 @@ for iSubj = 1:length(pth_subjdirs)
                 fprintf(fid, 'motion_fname_from_image_fname: 0\n' );
                 % 1/0: derive motion filename from data filename
                 fprintf(fid, 'use_diff_motion: 1\n' );
-                
+
                 %set spm_file_out = `ls ${long_name}/results_unwarp/SPM.mat`
                 % location of SPM.mat file (comment this line if you do not wish to estimate number of outliers per condition)
                 %fprintf(fid, 'spm_file: ${spm_file_out} ' >> ${basename}_art.cfg
@@ -115,20 +115,20 @@ for iSubj = 1:length(pth_subjdirs)
                 fprintf(fid, 'output_dir: %s\n', raw_dir  );
                 fprintf(fid, 'image_dir: %s\n', raw_dir  );          % functional and movement data folders (comment these lines if functional/movement filenames below contain full path information)
                 fprintf(fid, 'motion_dir: %s\n', raw_dir  );
-                
+
                 rp_file = rdir([raw_dir, filesep, 'rp_','*txt']);
                 rpIx = textscan(rp_file(1,1).name,'%s','Delimiter', '/');
                 rp_file = rpIx{1,1}{end};
                 if ~isempty(rp_file)
                     fprintf(fid, 'end\n\n'); %needed halfway through for ART script
-                    nameIx =  rdir([raw_dir, filesep, 'w*']);                
+                    nameIx =  rdir([raw_dir, filesep, 'w*']);
                     findShort = cellfun(@(x) numel(x), {nameIx.name}); % don't want to have the wmean file interrupting the pipeline, if it exists.
                     nameIx = nameIx(findShort==min(findShort));
-                    
-                    [ ~ , img_file, ext] = fileparts(nameIx.name);
 
-                    
-                    fprintf(fid, 'session 1 image %s.nii\n', img_file); %CHECK THE NUMBER OF ???'s if you are having trouble.
+                    [ ~ , img_file, ext] = fileparts(nameIx.name);
+                    repQs = repmat('?',1,length(img_file)-5);
+
+                    fprintf(fid, 'session 1 image %s%s.nii\n', img_file(1:5),repQs); %CHECK THE NUMBER OF ???'s if you are having trouble.
                     fprintf(fid, 'session 1 motion %s\n\n', rp_file );
                     %fprintf(fid, 'output_dir: %s\n', raw_dir ); %Don't send to cfg file
                     %    fprintf(fid, 'stats_dir:  ${long_name}'
